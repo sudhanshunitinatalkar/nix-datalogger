e82e8a721735a99d0f1c551e9ccddfb1b4412365
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2025-11-28 15:27:11 +0530

    update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "RPi Home Manager Config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = 
     {
       url = "github:nix-community/home-manager";
